77005bdf114a44183a44150eeb1b0753e31b3e69
Michael Thomas <m-thomas89@hotmail.com>
2020-11-06 21:13:25 -0500

    all code

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,13 @@
 var mysql = require('mysql');
-var connection;
 
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
+
+var connection = mysql.createConnection({
     host: 'jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user: 'wq1u3k8h5s3ivju1',
     password: 'o8nibv7y27s0efvc',
     database: 'xzoc3rqm6tzhxhnx'
   });
-};
-
+  
 connection.connect(function(err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
